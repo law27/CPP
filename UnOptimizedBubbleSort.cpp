c8bd6a6bce05fca8408b9beb6c1332e265f805d3
Harshitha <harshiling@gmail.com>
2019-06-05 12:32:21 +0530

    Update UnOptimizedBubbleSort.cpp

--- UnOptimizedBubbleSort.cpp
+++ UnOptimizedBubbleSort.cpp
@@ -18,3 +18,39 @@ int main()
   for(int i=0; i<len ; i++)
   cout<<arr[i]<<" ";
 }
+                                           /*********Harshi**********/
+                    
+                                                //Unoptimized
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+  int length ;
+  cout<<"Enter the number of array elements\n";
+  cin>>length;  
+
+  int arr[length];
+
+  for(int i = 0; i <length ; i++)
+  {
+  cout<<"Enter the element\n";
+  cin>>arr[i];
+              }
+
+  for(int startindex = 0; startindex<length; startindex++)
+  {
+    for(int currentindex = startindex+1; currentindex<length ; currentindex++)
+     {
+     if(arr[currentindex]>arr[startindex])
+
+     swap(arr[startindex],arr[currentindex])  ;
+     }
+     }
+
+     for(int index =0; index<length ; index++)
+     cout<<arr[index] << " ";
+
+
+      return 0;
+     }
