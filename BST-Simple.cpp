2bf0b883f4dcd48a7b30c9ad14d23dab673ba111
jeyalawrance@gmail.com <jeyalawrance@gmail.com>
2019-09-13 20:42:25 +0530

    BST with most of the Functions enabled

--- BST-Simple.cpp
+++ BST-Simple.cpp
@@ -31,7 +31,7 @@ private:
     }
     bool Searching(node*temp, int data)
     {
-        if(root==NULL)
+        if(temp==NULL)
             return false;
         else if(temp->data == data)
             return true;
@@ -53,12 +53,72 @@ public:
     {
         node* temp = root;
         bool l = Searching(temp,data);
+        if(l)
+            cout<<"Found"<<endl;
+        else
+            cout<<"Not Found"<<endl;
+    }
+    node* Get_Root(){
+        node* temp = root;
+        return temp;
     }
 };
 
+
+void Inorder_Traversal(node* ptr){
+    if(ptr!=NULL)
+ {   Inorder_Traversal(ptr->left);
+
+    cout<<ptr->data<<"  ";
+
+    Inorder_Traversal(ptr->right);}
+}
+
+void PreOrder_Traversal(node* ptr){
+    if(ptr!=NULL)
+    {
+        cout<<ptr->data<<"  ";
+        PreOrder_Traversal(ptr->left);
+        PreOrder_Traversal(ptr->right);
+    }
+}
+
+void PostOrder_Traversal(node* ptr){
+    if(ptr!=NULL)
+    {
+        PostOrder_Traversal(ptr->left);
+        PostOrder_Traversal(ptr->right);
+        cout<<ptr->data<<"  ";
+    }
+}
+
 int main()
 {
-    
+    BST a;
+    int q;
+    cout<<"How many elements you have to Enter into the BST ?"<<endl;
+    cin>>q;
+    int b;
+    for(int i=0; i<q; i++)
+    {
+        cout<<"Enter the "<<i+1<<" Element"<<endl;
+        cin>>b;
+        a.Insert(b);
+        cout<<"Element Inserted into the Tree"<<endl;
+    }
+    node* temp = a.Get_Root();
+    cout<<"The Inorder Traversal is "<<endl;
+    Inorder_Traversal(temp);
+    cout<<endl;
+    cout<<"The Preorder Traversal is "<<endl;
+    PreOrder_Traversal(temp);
+    cout<<endl;
+    cout<<"The Postorder Traversal is "<<endl;
+    PostOrder_Traversal(temp);
+    cout<<endl;
+    return 0;
 }
+
+
 
 
