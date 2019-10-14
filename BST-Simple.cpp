624b5ed1f2ec61af6e6f54bd5eb05e459e7cc77a
jeyalawrance@gmail.com <jeyalawrance@gmail.com>
2019-10-14 20:52:07 +0530

    BST-Simple.cpp

--- BST-Simple.cpp
+++ BST-Simple.cpp
@@ -1,123 +1,179 @@
 #include<iostream>
 using namespace std;
 
-struct node{
+struct node
+{
     int data;
     node* right;
     node* left;
 };
 
-class BST{
+class BST
+{
 private:
-    node *root;
-    node* Create_Node(int data)
+    node* root = new node();
+
+    node* New_Node(int data)
     {
-        node* temp = new node;
+        node* temp = new node();
         temp->data = data;
-        temp->left = temp->right = NULL;
+        temp->right = NULL;
+        temp->left = NULL;
         return temp;
     }
-    node* Insert(node* temp, int data)
+
+    node* Insert(int data,node* ptr)
     {
-        if(temp==NULL && root==NULL)
-            root = Create_Node(data);
-        if(temp==NULL)
-            temp = Create_Node(data);
-        else if(data <= temp->data)
-            temp->left = Insert(temp->left,data);
+        if(ptr==NULL && root==NULL)
+        {
+            root = New_Node(data);
+        }
+        if(ptr==NULL)
+        {
+            ptr = New_Node(data);
+        }
+        else if(data > ptr->data)
+        {
+            ptr->right = Insert(data, ptr->right);
+        }
         else
-            temp->right = Insert(temp->right,data);
-        return temp;
+        {
+            ptr->left = Insert(data, ptr->left);
+        }
+        return ptr;
     }
-    bool Searching(node*temp, int data)
+    string Searching(int data,node* ptr)
     {
-        if(temp==NULL)
-            return false;
-        else if(temp->data == data)
-            return true;
-        else if(data<=temp->data)
-            return Searching(temp->left,data);
+        if (ptr == NULL)
+            return "NOT FOUND";
+        if (ptr->data == data)
+            return "FOUND";
+        else if (data > ptr->data)
+            return Searching(data, ptr->right);
         else
-            return Searching(temp->right,data);
+            return Searching(data, ptr->left);
     }
+
+    void In_Order_traversal(node* ptr)
+    {
+        if(ptr!=NULL)
+        {
+            In_Order_traversal(ptr->left);
+            cout << ptr->data << "  ";
+            In_Order_traversal(ptr->right);
+        }
+    }
+    void Post_Order_traversal(node* ptr)
+    {
+        if (ptr != NULL)
+        {
+            cout << ptr->data << "  ";
+            Post_Order_traversal(ptr->left);
+            Post_Order_traversal(ptr->right);
+        }
+    }
+    void Pre_Order_traversal(node* ptr)
+    {
+        if (ptr != NULL)
+        {
+            Post_Order_traversal(ptr->left);
+            Post_Order_traversal(ptr->right);
+            cout << ptr->data << "  ";
+        }
+    }
+    
 public:
-    BST(){
-    root = NULL;
+    BST()
+    {
+        root = NULL;
     }
     void Insert(int data)
     {
         node* temp = root;
-        Insert(temp,data);
+        Insert(data, temp);
     }
     void Searching(int data)
     {
-        node* temp = root;
-        bool l = Searching(temp,data);
-        if(l)
-            cout<<"Found"<<endl;
-        else
-            cout<<"Not Found"<<endl;
+        cout << Searching(data, root) << endl;
     }
-    node* Get_Root(){
-        node* temp = root;
-        return temp;
+    void In_Order_Traversal()
+    {
+        In_Order_traversal(root);
+    }
+    void Post_Order_traversal()
+    {
+        Post_Order_traversal(root);
+    }
+    void Pre_Order_traversal()
+    {
+        Pre_Order_traversal(root);
     }
 };
-
-
-void Inorder_Traversal(node* ptr){
-    if(ptr!=NULL)
- {   Inorder_Traversal(ptr->left);
-
-    cout<<ptr->data<<"  ";
-
-    Inorder_Traversal(ptr->right);}
-}
-
-void PreOrder_Traversal(node* ptr){
-    if(ptr!=NULL)
-    {
-        cout<<ptr->data<<"  ";
-        PreOrder_Traversal(ptr->left);
-        PreOrder_Traversal(ptr->right);
-    }
-}
-
-void PostOrder_Traversal(node* ptr){
-    if(ptr!=NULL)
-    {
-        PostOrder_Traversal(ptr->left);
-        PostOrder_Traversal(ptr->right);
-        cout<<ptr->data<<"  ";
-    }
-}
 
 int main()
 {
     BST a;
-    int q;
-    cout<<"How many elements you have to Enter into the BST ?"<<endl;
-    cin>>q;
-    int b;
-    for(int i=0; i<q; i++)
+    cout << "How many nodes you have to Enter in the BST?" << endl;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++)
     {
-        cout<<"Enter the "<<i+1<<" Element"<<endl;
-        cin>>b;
-        a.Insert(b);
-        cout<<"Element Inserted into the Tree"<<endl;
+        int number;
+        cin >> number;
+        a.Insert(number);
+        cout << "Element is added to the Linked_list" << endl;
     }
-    node* temp = a.Get_Root();
-    cout<<"The Inorder Traversal is "<<endl;
-    Inorder_Traversal(temp);
-    cout<<endl;
-    cout<<"The Preorder Traversal is "<<endl;
-    PreOrder_Traversal(temp);
-    cout<<endl;
-    cout<<"The Postorder Traversal is "<<endl;
-    PostOrder_Traversal(temp);
-    cout<<endl;
+    while(true)
+    {
+        cout << "Enter the Element you have to Search is BST" << endl;
+        int num;
+        cin >> num;
+        a.Searching(num);
+        if (num == 0)
+            break;
+    }
+    cout<<"In-Order Traversal"<<endl;
+    a.In_Order_Traversal();
+    cout << endl;
+    cout << "Post-Order Traversal" << endl;
+    a.Post_Order_traversal();
+    cout << endl;
+    cout << "Pre-Order Traversal" << endl;
+    a.Pre_Order_traversal();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
