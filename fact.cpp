fc33207dba7105159efdc9022385b3423cf31e2f
Harshitha <harshiling@gmail.com>
2019-10-26 17:26:57 +0530

    Factorial of large numbers

--- fact.cpp
+++ fact.cpp
@@ -1,36 +1,38 @@
+
+//Factorial of Large numbers in CPP
 #include<iostream>
 using namespace std;
 
 int main()
 { 
-  int T;
+  int T; // Testcase
   cin>>T;
   while(T--){
-  int arr[200];
-  int N;
+  int arr[200]; // For storing max numbers of digit4*3*
+  int N; //The number to find its factorial
   cin>>N;
-  arr[0] = 1;
-  int m = 1;
+  arr[0] = 1; //Digit 1
+  int m = 1; //The size of array
   int carry = 0;
-  for(int x = 2 ; x <= N ; x++)
+  for(int x = 2 ; x <= N ; x++) //factorial calculation starts ends with 2 ( 4! = 4*3*2 ) 
   {
-  for(int i = 0 ; i<m ; i++)
+  for(int i = 0 ; i<m ; i++)  // Traversing from the needed digits
   {
-      int prod = arr[i] * x + carry;
-      arr[i] = prod %10;
-      carry = prod/10;
+      int prod = arr[i] * x + carry; // 1 * 2 + 0 = 2
+      arr[i] = prod %10;  //remainder
+      carry = prod/10;     // quotient
   }
-  while(carry)
-  {
-     arr[m] = carry%10;
-     carry = carry/10;
+  while(carry)   // while carry remains ( carry != 0)
+  { 
+     arr[m] = carry%10;    //store rem-carry in array of m size
+     carry = carry/10;   //divide carry by itself
   
-  m++;
+  m++;   //Increase sixe of carry
   }
   }
-
+//For Printing , its backwards !!
   for(int i = m-1 ; i >= 0 ; i--)
-  {
+  {  cout<<"The factorial of <<" "<<N<<"is"<<endl;              
      cout<<arr[i]<<" ";
   }
 
