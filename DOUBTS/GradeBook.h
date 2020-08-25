fa49b7ff56bb46c6fe7dad6d395fb6aa5e745b23
Harshitha <harshiling@gmail.com>
2020-08-25 19:49:33 +0530

    Rename DOUBTS/Day-01/GradeBook.h to DOUBTS/GradeBook.h

---
+++ DOUBTS/GradeBook.h
@@ -0,0 +1,18 @@
+
+//Gradebook.h
+//Interface
+#include<iostream>
+using namespace std;
+
+class GradeBook
+{
+private:
+    string courseName;
+public:
+   GradeBook (string );
+   void setCourseName(string);
+   string getCourseName();
+   void displayCourseName();
+};
+
+
